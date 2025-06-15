90d405d01dda0a6862c315b8cc4c24424645a0f9
Gurpreet Singh <gurpreets705@gmail.com>
2025-06-16 01:53:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Importing global styles
-import App from './App'; // Importing your main App component
+import 'index.css'; // Importing global styles
+import App from 'App'; // Importing your main App component
 
 // Find the root div from the public/index.html file
 const root = ReactDOM.createRoot(document.getElementById('root'));
