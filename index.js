2afe9c8dad779c1a495046b332c5272bec4fc5ec
Gurpreet Singh <gurpreets705@gmail.com>
2025-06-16 00:40:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Importing global styles
+import App from './App'; // Importing your main App component
+
+// Find the root div from the public/index.html file
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your App component into the root div
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
